10f2068eef8c595292eb44e2a5dc913a1de8ff40
Ivan Beňovic <ivan.benovic@innovatrics.com>
2020-03-19 13:32:10 +0100

    Fixed build error [SMAR-762].

--- sfcli/SmartFace.CliTests/SfCliTests/Domain/GlobalConfig/GlobalConfigTests.cs
+++ sfcli/SmartFace.CliTests/SfCliTests/Domain/GlobalConfig/GlobalConfigTests.cs
@@ -8,11 +8,11 @@ using Tests;
 
 namespace SmartFace.CliTests.SfCliTests.Domain.GlobalConfig
 {
-    
+
     public class GlobalConfigContext
     {
         public SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig Config { get; set; }
-        
+
         public IGlobalConfigRepository Repository { get; }
 
         public IApiProvider ApiProvider { get; } = Tests.Utils.SubstituteApiProvider();
@@ -21,12 +21,12 @@ namespace SmartFace.CliTests.SfCliTests.Domain.GlobalConfig
         {
             Repository = new GlobalConfigRepository(ApiProvider, Installer.ConfigureAutoMapper());
         }
-        
+
     }
-    
+
     public class SetGlobalConfig_IFaceConfigCamera : Scenario<GlobalConfigContext>
     {
-        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ GpuEnabled = true };
+        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { GpuEnabled = true };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
@@ -34,10 +34,10 @@ namespace SmartFace.CliTests.SfCliTests.Domain.GlobalConfig
             _.ApiProvider.IFaceConfigs.Received().Set(Arg.Is(IFaceConfigKey.Camera),
                 Arg.Is<IFaceConfigModel>((model) => model.GpuEnabled == _.Config.GpuEnabled));
     }
-    
+
     public class SetGlobalConfig_IFaceConfigDetect : Scenario<GlobalConfigContext>
     {
-        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ GpuEnabled = true };
+        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { GpuEnabled = true };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
@@ -45,10 +45,10 @@ namespace SmartFace.CliTests.SfCliTests.Domain.GlobalConfig
             _.ApiProvider.IFaceConfigs.Received().Set(Arg.Is(IFaceConfigKey.Detect),
                 Arg.Is<IFaceConfigModel>((model) => model.GpuEnabled == _.Config.GpuEnabled));
     }
-    
+
     public class SetGlobalConfig_IFaceConfigExtract : Scenario<GlobalConfigContext>
     {
-        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ GpuEnabled = true };
+        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { GpuEnabled = true };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
@@ -56,10 +56,10 @@ namespace SmartFace.CliTests.SfCliTests.Domain.GlobalConfig
             _.ApiProvider.IFaceConfigs.Received().Set(Arg.Is(IFaceConfigKey.Extract),
                 Arg.Is<IFaceConfigModel>((model) => model.GpuEnabled == _.Config.GpuEnabled));
     }
-    
+
     public class SetGlobalConfig_IFaceConfigGrouping : Scenario<GlobalConfigContext>
     {
-        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ GpuEnabled = true };
+        private given _gpu_enabled = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { GpuEnabled = true };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
@@ -67,40 +67,40 @@ namespace SmartFace.CliTests.SfCliTests.Domain.GlobalConfig
             _.ApiProvider.IFaceConfigs.Received().Set(Arg.Is(IFaceConfigKey.Grouping),
                 Arg.Is<IFaceConfigModel>((model) => model.GpuEnabled == _.Config.GpuEnabled));
     }
-    
+
     public class SetGlobalConfig_FaceHandler_MinEye : Scenario<GlobalConfigContext>
     {
-        private given _min_eye = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ DetectionMinEyeDistance = 43 };
+        private given _min_eye = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { DetectionMinEyeDistance = 43 };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
         private then _fh_config_min_eye_distance_is_set = _ =>
             _.ApiProvider.FaceHandlerConfigs.Received().Set(Arg.Is<FaceHandlerConfigModel>((model) => model.DetectionMinEyeDistance == _.Config.DetectionMinEyeDistance));
     }
-    
+
     public class SetGlobalConfig_FaceHandler_MaxEye : Scenario<GlobalConfigContext>
     {
-        private given _max_eye = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ DetectionMaxEyeDistance = 2345 };
+        private given _max_eye = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { DetectionMaxEyeDistance = 2345 };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
         private then _fh_config_max_eye_distance_is_set = _ =>
             _.ApiProvider.FaceHandlerConfigs.Received().Set(Arg.Is<FaceHandlerConfigModel>((model) => model.DetectionMaxEyeDistance == _.Config.DetectionMaxEyeDistance));
     }
-    
+
     public class SetGlobalConfig_FaceHandler_Confidence : Scenario<GlobalConfigContext>
     {
-        private given _confidence = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ FaceConfidenceThreshold = 543 };
+        private given _confidence = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { FaceConfidenceThreshold = 543 };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
         private then _fh_config_confidence_is_set = _ =>
             _.ApiProvider.FaceHandlerConfigs.Received().Set(Arg.Is<FaceHandlerConfigModel>((model) => model.FaceConfidenceThreshold == _.Config.FaceConfidenceThreshold));
     }
-    
+
     public class SetGlobalConfig_FaceHandler_DetectionAlgorithm : Scenario<GlobalConfigContext>
     {
-        private given _algorithm_gpu = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig{ DetectionAlgorithm = DetectionAlgorithm.Gpu };
+        private given _algorithm_gpu = _ => _.Config = new SmartFace.Cli.Core.Domain.GlobalConfig.GlobalConfig { DetectionAlgorithm = DetectionAlgorithm.Balanced };
 
         private when _set_config = _ => _.Repository.Set(_.Config);
 
