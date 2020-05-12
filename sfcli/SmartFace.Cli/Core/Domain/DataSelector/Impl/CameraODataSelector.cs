62ab37b59e45a73cc54c33381cc03752b6b644d9
Adam Blasko <adam.blasko@innovatrics.com>
2020-05-12 16:07:34 +0200

    using instead of full qualifier

--- sfcli/SmartFace.Cli/Core/Domain/DataSelector/Impl/CameraODataSelector.cs
+++ sfcli/SmartFace.Cli/Core/Domain/DataSelector/Impl/CameraODataSelector.cs
@@ -1,8 +1,9 @@
 using SmartFace.ODataClient.Default;
+using SmartFace.ODataClient.SmartFace.Domain.DataAccess.Models.Core;
 
 namespace SmartFace.Cli.Core.Domain.DataSelector.Impl
 {
-    public class CameraODataSelector : ODataSelector<ODataClient.SmartFace.Domain.DataAccess.Models.Core.Camera>, IQueryDataSelector<ODataClient.SmartFace.Domain.DataAccess.Models.Core.Camera>
+    public class CameraODataSelector : ODataSelector<Camera>, IQueryDataSelector<Camera>
     {
         public CameraODataSelector(Container container) : base(container.Cameras)
         {
