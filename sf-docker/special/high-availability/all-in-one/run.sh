06a2a9b7dd16685c25debcacf24c2bfa5773e38c
Lukas Malik <lukasmalik11@gmail.com>
2023-04-20 13:07:31 +0200

    WIP

---
+++ sf-docker/special/high-availability/all-in-one/run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ ! -f iengine.lic ]; then
+    echo "License file not found. Please make sure that the license file is present in the current directory." >&2
+    exit 1
+fi
+
+COMPOSE_COMMAND="docker compose"
+
+set +e
+
+$COMPOSE_COMMAND version
+
+if [ $? -ne 0 ]; then
+    COMPOSE_COMMAND="docker-compose"
+    $COMPOSE_COMMAND version
+    if [ $? -ne 0 ]; then
+        echo "No compose command found. Please install docker compose" >&2
+        exit 1
+    fi
+fi
+
+set -e
+# sf-network is used so that sf-dependencies and sf containers can communicate
+# this can fail if the network already exists, but we don't mind that
+docker network create sf-network || true
+
+# start dependencies of SF - MsSql, RMQ and minio
+$COMPOSE_COMMAND -f sf_dependencies/docker-compose.yml up -d
+
+# sleep to wait for the dependencies to start up
+sleep 10
+
+# load version and registry from .env
+VERSION=$(grep -E ^SF_VERSION .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+REGISTRY=$(grep -E ^REGISTRY .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+
+# we use the DB engine that will be used by SF to create and migrate the DB
+# to switch DB engine, change the .env file
+DB_ENGINE=$(grep -E ^Database__DbEngine .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+
+# load RabbitMQ properties from .env
+RMQ_HOST=$(grep -E ^RabbitMQ__Hostname .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+RMQ_USER=$(grep -E ^RabbitMQ__Username .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+RMQ_PASS=$(grep -E ^RabbitMQ__Password .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+RMQ_VHOST=$(grep -E ^RabbitMQ__VirtualHost .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+RMQ_PORT=$(grep -E ^RabbitMQ__Port .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+RMQ_SSL=$(grep -E ^RabbitMQ__UseSsl .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+
+S3_ENDPOINT=$(grep -E ^S3Bucket__Endpoint .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+S3_ACCESS=$(grep -E ^S3Bucket__AccessKey .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+S3_SECRET=$(grep -E ^S3Bucket__SecretKey .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+S3_BUCKET=$(grep -E ^S3Bucket__BucketName .env | cut -d '=' -f2 | cut -d$'\r' -f1)
+# set correct hostname to sfstation env file
+sed -i "s/S3_ENDPOINT=.*/S3_ENDPOINT=http:\/\/$(hostname):9000/g" .env.sfstation
+
+echo $VERSION
+echo $REGISTRY
+
+# create mqtt user for rmq mqtt plugin
+docker exec -it rmq /opt/rabbitmq/sbin/rabbitmqctl add_user mqtt mqtt || true
+docker exec -it rmq /opt/rabbitmq/sbin/rabbitmqctl set_user_tags mqtt administrator || true
+docker exec -it rmq /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p "/" mqtt ".*" ".*" ".*" || true
+
+if [[ "$DB_ENGINE" == "MsSql" ]]; then
+    # create SmartFace database in MsSql
+    docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Test1234 -Q "CREATE DATABASE SmartFace" || true
+    # run database migration to current version
+    docker run --rm --name admin_migration --network sf-network ${REGISTRY}sf-admin:${VERSION} run-migration -p 5 -c "Server=mssql;Database=SmartFace;User ID=sa;Password=Test1234;" -dbe $DB_ENGINE --rmq-host ${RMQ_HOST} --rmq-user ${RMQ_USER} --rmq-pass ${RMQ_PASS} --rmq-virtual-host ${RMQ_VHOST} --rmq-port ${RMQ_PORT} --rmq-use-ssl ${RMQ_SSL}
+elif [[ "$DB_ENGINE" == "PgSql" ]]; then
+    # create SmartFace database in PgSql
+    docker exec pgsql psql -U postgres -c "CREATE DATABASE smartface" || true
+    # run database migration to current version
+    docker run --rm --name admin_migration --network sf-network ${REGISTRY}sf-admin:${VERSION} run-migration -p 5 -c "Server=pgsql;Database=smartface;Username=postgres;Password=Test1234;Trust Server Certificate=true;" -dbe $DB_ENGINE --rmq-host ${RMQ_HOST} --rmq-user ${RMQ_USER} --rmq-pass ${RMQ_PASS} --rmq-virtual-host ${RMQ_VHOST} --rmq-port ${RMQ_PORT} --rmq-use-ssl ${RMQ_SSL}
+else
+    echo "Unknown DB engine: ${DB_ENGINE}!" >&2
+    exit 1
+fi
+
+docker run --rm --name s3-bucket-create --network sf-network ${REGISTRY}sf-admin:${VERSION} ensure-s3-bucket-exists --endpoint "$S3_ENDPOINT" --access-key "$S3_ACCESS" --secret-key  "$S3_SECRET" --bucket-name "$S3_BUCKET"
+
+# finally start SF images
+$COMPOSE_COMMAND up -d --force-recreate
