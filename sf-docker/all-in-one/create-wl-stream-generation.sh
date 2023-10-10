fc3473e3c8c0ffaa67a307c521a2152aab6a90df
Peter Pokojny <peter.pokojny@innovatrics.com>
2023-10-10 08:24:20 +0200

    Updated WL stream generation script

--- sf-docker/all-in-one/create-wl-stream-generation.sh
+++ sf-docker/all-in-one/create-wl-stream-generation.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-source .env
+getvalue() {
+    local key="$1"
+    local value=$(grep -E ^${key}= .env | cut -d '=' -f2- | cut -d$'\r' -f1)
+    echo "$value"
+}
 
-docker run --rm --name admin-stream-gen --network sf-network ${REGISTRY}sf-admin:${SF_VERSION} \
+docker run --rm --name admin-stream-gen --network sf-network $(getvalue REGISTRY)sf-admin:$(getvalue SF_VERSION) \
     populate-wl-update-log-stream \
-    -c "${ConnectionStrings__CoreDbContext}" \
-    -dbe ${Database__DbEngine} \
-    --rmq-host ${RabbitMQ__Hostname} --rmq-user ${RabbitMQ__Username} --rmq-pass ${RabbitMQ__Password} \
-    --rmq-virtual-host ${RabbitMQ__VirtualHost} --rmq-port ${RabbitMQ__Port} --rmq-use-ssl ${RabbitMQ__UseSsl} --rmq-streams-port ${RabbitMQ__StreamsPort}
+    -c "$(getvalue ConnectionStrings__CoreDbContext)" \
+    -dbe $(getvalue Database__DbEngine) \
+    --rmq-host $(getvalue RabbitMQ__Hostname) --rmq-user $(getvalue RabbitMQ__Username) --rmq-pass $(getvalue RabbitMQ__Password) \
+    --rmq-virtual-host $(getvalue RabbitMQ__VirtualHost) --rmq-port $(getvalue RabbitMQ__Port) --rmq-use-ssl $(getvalue RabbitMQ__UseSsl) --rmq-streams-port $(getvalue RabbitMQ__StreamsPort)
