b85e204c7c2c5dac88eeb2ee1b0c7d8c57432933
luke-1988 <lukasmalik11@gmail.com>
2024-08-08 14:18:00 +0200

    onsite

--- sf-docker/sf_observability/kill.sh
+++ sf-docker/sf_observability/kill.sh
@@ -2,7 +2,7 @@ docker kill $(docker ps -a -q)
 
 docker rm $(docker ps -a -q)
 
-docker volume prune --force --all
-docker network prune --force --all
+docker volume prune --force
+docker network prune --force
 
 git fetch -p; git pull;
