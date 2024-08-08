321bfbd7b9aaf685217f851a734c57937ffaa89a
luke-1988 <lukasmalik11@gmail.com>
2024-08-08 14:10:23 +0200

    onsite

--- sf-docker/sf_observability/kill.sh
+++ sf-docker/sf_observability/kill.sh
@@ -2,6 +2,6 @@ docker kill $(docker ps -a -q)
 
 docker rm $(docker ps -a -q)
 
-docker system prune --force --all
+docker volume prune --force --all
 
 git fetch -p; git pull;
--- sf-docker/sf_observability/run.sh
+++ sf-docker/sf_observability/run.sh
@@ -35,3 +35,10 @@ docker run --rm --network sf-observability --entrypoint /bin/sh minio/mc -c "/us
 #docker run --rm --network sf-observability --entrypoint /bin/sh minio/mc -c "/usr/bin/mc policy set public obsminio/loki"
 docker run --rm --network sf-observability --entrypoint /bin/sh minio/mc -c "/usr/bin/mc mc anonymous set public obsminio/loki"
 
+
+docker run --rm --network sf-observability --entrypoint /bin/sh minio/mc -c "
+      /usr/bin/mc config host add obsminio http://obsminio:9000 minioadmin minioadmin;
+      /usr/bin/mc mb obsminio/loki;
+      /usr/bin/mc policy set public obsminio/loki;
+      exit 0;
+      "
