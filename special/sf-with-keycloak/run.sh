afe5fe3311c24828293008b0efd2aff58ac09cf2
Lukas Malik <37071890+Luke-1988@users.noreply.github.com>
2026-05-20 21:40:01 +0200

    Grant read permissions to Keycloak realm export file in run.sh for proper container access.

--- special/sf-with-keycloak/run.sh
+++ special/sf-with-keycloak/run.sh
@@ -5,6 +5,9 @@ set -e
 
 docker network create sf-network || true
 
+# Keycloak container runs as UID 1000 (jboss) and needs to read the realm export
+chmod a+r keycloak-server/realm-export.json
+
 docker compose -f keycloak-server/docker-compose.yml up -d
 docker compose -f nginx/docker-compose.yml up -d
 
